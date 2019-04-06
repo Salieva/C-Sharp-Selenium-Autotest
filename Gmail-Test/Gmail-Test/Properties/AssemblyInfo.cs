8fc1e5b7947bf260c39f193114684137d1ffba55
Salieva <okilasalieva@gmail.com>
2019-04-06 14:17:29 -0700

    gmail email word search
    Search for 'done' in Dennis's email

---
+++ Gmail-Test/Gmail-Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gmail-Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Gmail-Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5008b94-5f18-43db-9324-ccff6f001700")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Gmail-Test/Gmail-Test/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Gmail_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url="http:\\gmail.com";
+            IWebElement element = driver.FindElement(By.XPath("//input[@id='identifierId']"));
+            element.SendKeys("oselenium");
+            element = driver.FindElement(By.XPath("//div[@id='identifierNext']"));
+            element.Click();
+            element = driver.FindElement(By.XPath("//input[@name='password']"));
+            element.SendKeys("Dennis_345");
+            driver.FindElement(By.XPath("//div[@id='passwordNext']")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[@email='DENNIS.DIDULO@bd.com']")));
+            element = driver.FindElement(By.XPath("//span[@email='DENNIS.DIDULO@bd.com']"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", element);
+            By loc = (By.XPath("//p[contains(text(), 'done')]"));
+            Assert.IsTrue(IsElementPresent(driver, loc));
+            System.Threading.Thread.Sleep(5000);
+            driver.Quit();
+
+            bool IsElementPresent(IWebDriver drv, By locator)
+            {
+                try
+                {
+                    drv.FindElement(locator);
+                    return true;
+                }
+                catch (NoSuchElementException ex)
+                {
+                    return false;
+                }
+            }
+
+            
+
+
+
+
+            driver.Quit();
+
+
+
+
+
+        }
+    }
+}
